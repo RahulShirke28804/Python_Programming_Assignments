c3a1b25205f70956c0f2ffb305e2fe264c346d2f
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-23 19:40:20 +0530

    Update Program4.py

--- Assignment_13/Program4.py
+++ Assignment_13/Program4.py
@@ -1,21 +1,33 @@
 '''
-    4. Write a program which accepts one number and prints that many numbers starting from 1.
-    Input: 5
-    Output: 1 2 3 4 5
+    4. Write a program which accepts one number and prints binary equivalent.
 
 '''
 
-def Display(No):
-    for i in range(1,No+1):
-        print(i,end = " ")
+def Binary(No):
+    binary = list()
+    
+    while(No != 0):
+        if((No % 2) == 0):
+            binary.append(0)
+        else:
+            binary.append(1)
 
-    print()
+        No = No // 2
+
+    return binary
 
 def main(): 
+    Value = 0
+
     print("Enter the number : ")
     Value = int(input())
 
-    Display(Value)
+    Binary(Value)
+
+    ret = Binary(Value)
+
+    for i in range(len(ret) - 1, -1 ,-1):
+        print(ret[i],end=" ")
 
 if __name__ == "__main__":
     main()
