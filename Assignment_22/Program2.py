7d876d7750f8c7953debc7120e1045826fd4ecf8
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-30 23:40:37 +0530

    Add files via upload

---
+++ Assignment_22/Program2.py
@@ -0,0 +1,60 @@
+'''
+    2. Write a Python program to implement a class named Circle with the following
+    requirements:
+    • The class should contain three instance variables: Radius, Area, and Circumference.
+    • The class should contain one class variable named PI, initialized to 3.14.
+    • Define a constructor (__init__) that initializes all instance variables to 0.0.
+    • Implement the following instance methods:
+        ◦ Accept() : accepts the radius of the circle from the user.
+        ◦ CalculateArea() : calculates the area of the circle and stores it in the Area variable.
+        ◦ CalculateCircumference() : calculates the circumference of the circle and stores it in 
+        the Circumference variable.
+        ◦ Display() : displays the values of Radius, Area, and Circumference.
+    • Create multiple objects of the Circle class and invoke all the instance methods for each object.
+
+'''
+
+class Circle:
+    PI = 3.14
+
+    def __init__(self,):
+        self.Radius = 0.0
+        self.Area = 0.0
+        self.Circumference = 0.0
+
+    def Accept(self):
+        print("Enter the radius of the circle : ")
+        self.Radius = float(input())
+
+    def CalculateArea(self):
+        self.Area = self.PI * self.Radius *self.Radius
+
+    def CalculateCircumference(self):
+        self.Circumference = 2 * self.PI * self.Radius
+
+    def Display(self):
+        print("Radius of the circle is : ",self.Radius)  
+        print(f"Area of the circle with radius {self.Radius} is : ",self.Area)  
+        print(f"Circumference of the circle with radius {self.Radius} is : ",self.Circumference)  
+    
+def main():
+
+    obj1 = Circle()
+    obj2 = Circle()
+
+    obj1.Accept()
+    obj1.CalculateArea()
+    obj1.CalculateCircumference()
+    obj1.Display()
+
+    obj2.Accept()
+    obj2.CalculateArea()
+    obj2.CalculateCircumference()
+    obj2.Display()
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
